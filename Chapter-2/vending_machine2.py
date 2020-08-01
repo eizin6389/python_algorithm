9232373ee0d4a6aeb7d7f42d32383e1d0c544c36
松本英人 <hide@matsumotohideto-no-MacBook-Pro.local>
2020-08-01 23:46:27 +0900

    git rm -r --cached . after all data git add

---
+++ Chapter-2/vending_machine2.py
@@ -0,0 +1,33 @@
+insert_price = input('insert_price: ')
+product_price = input('product_price: ')
+change = int(insert_price) - int(product_price)
+
+r5000 = change // 5000
+q5000 = change % 5000
+print('5000: '+str(r5000))
+
+r1000 = q5000 // 1000
+q1000 = q5000 % 1000
+print('1000: '+str(r1000))
+
+r500 = q1000 // 500
+q500 = q1000 % 500
+print('500: '+str(r500))
+
+r100 = q500 // 100
+q100 = q500 % 100
+print('100: '+str(r100))
+
+r50 = q100 // 50
+q50 = q100 % 50
+print('50: '+str(r50))
+
+r10 = q50 // 10
+q10 = q50 % 10
+print('10: '+str(r10))
+
+r5 = q10 // 5
+q5 = q10 % 5
+print('5: '+str(r5))
+
+print('1: '+str(q5))
